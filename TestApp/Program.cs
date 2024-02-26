2a0391310b84a0df85d139abfa6511a86f14ef5a
Ashhad <mianashhad7@gmail.com>
2024-02-26 20:30:14 +0500

    Tried switch statements

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,25 +1,56 @@
-﻿/*
-This code reverses a message, counts the number of
-times a character appears in the message and then
-prints the result on the screen.
-*/
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-string str = "The quick brown fox jumps over the lazy dog.";
+string[] product = sku.Split('-');
 
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
-
-int x = 0;
-
-foreach (char i in charMessage)
+string type = "";
+string color = "";
+string size = "";
+switch (product[0])
 {
-    if (i == 'o')
-    {
-        x++;
-    }
+    case("01"):
+        type = "Sweat shirt";
+        break;
+    case("02"):
+        type = "T-Shirt";
+        break;
+    case("03"):
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
 
-string new_message = new String(charMessage);
 
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
--- TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddea0cc98d038a56b86e9637bcf2bc360f76a703")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2041e4173170b31a539e2430afdfcbd99f17eefc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
